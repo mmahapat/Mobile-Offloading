95c1b4b9821a851966cba8694e206551a79ed0b3
Hemanshu Solanki <hemanshu116@gmail.com>
2020-05-01 21:08:58 -0700

    Add clients in ListView, Add rescan button, Update progress bar.

--- Master/app/src/main/java/com/mobilespark/master/ClientList.java
+++ Master/app/src/main/java/com/mobilespark/master/ClientList.java
@@ -5,7 +5,12 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.format.Formatter;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mobilespark.master.Pojos.ClientListData;
@@ -19,25 +24,46 @@ public class ClientList extends AppCompatActivity {
     private static final String TAG = "ClientList";
     private String localIp = "";
     private ListView list;
-    private ArrayList<ClientListData> clientData = null;
+    private ArrayList<ClientListData> clientData = new ArrayList<>();
+    private Button _startTaskButton;
+    private ImageButton _rescanButton;
+    ProgressBar pb;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_list);
-        clientData = new ArrayList<>();
         list = findViewById(R.id.nodelist);
+        _startTaskButton = findViewById(R.id.startTask);
+        _rescanButton = findViewById(R.id.rescan);
+        pb = findViewById(R.id.progress_horizontal);
+        textView = findViewById(R.id.percent);
 
         ClientListAdapter adapter = new ClientListAdapter(this, clientData);
         list.setAdapter(adapter);
         WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         localIp = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
 
-        try {
-            new NetworkDiscovery(localIp, list, clientData).execute();
-        } catch (Exception e) {
-            Log.e(TAG, "onCreate: " + e.getMessage());
-        }
+        new NetworkDiscovery(localIp, list).execute();
+
+        Log.e(TAG, "onCreate: " + clientData.size());
+
+        _rescanButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                _rescanButton.setVisibility(View.GONE);
+                clientData.clear();
+                ((ClientListAdapter) (list.getAdapter())).notifyDataSetChanged();
+                _startTaskButton.setEnabled(false);
+                pb.setProgress(0);
+                pb.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.VISIBLE);
+                textView.setText("Scanning : 0%");
+                new NetworkDiscovery(localIp, list).execute();
+            }
+        });
 
     }
 
@@ -50,49 +76,65 @@ public class ClientList extends AppCompatActivity {
     }
 
 
-    static class NetworkDiscovery extends AsyncTask<Void, ClientListData, Void> {
+    class NetworkDiscovery extends AsyncTask<Void, Object, Void> {
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            textView.setVisibility(View.GONE);
+            pb.setVisibility(View.GONE);
+            Toast.makeText(ClientList.this, "Scan Complete",
+                    Toast.LENGTH_LONG).show();
+            if (clientData.size() < 1) {
+                Toast.makeText(ClientList.this, "No Clients found, Please Rescan",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                _startTaskButton.setEnabled(true);
+            }
+            _rescanButton.setVisibility(View.VISIBLE);
+        }
 
         private static final String TAG = "NetworkDiscovery";
 
+
         @Override
-        protected void onProgressUpdate(ClientListData... values) {
+        protected void onProgressUpdate(Object... values) {
 //            super.onProgressUpdate(values);
-            clientData.add(values[0]);
-            ((ClientListAdapter) (listView.getAdapter())).notifyDataSetChanged();
+            if (values[0] != null) {
+                clientData.add((ClientListData) values[0]);
+                ((ClientListAdapter) (listView.getAdapter())).notifyDataSetChanged();
+            } else {
+                int percent = (int) values[1];
+                pb.setProgress(percent);
+                textView.setText("Scanning : " + percent + "%");
+            }
         }
 
         private String localIp;
-        ArrayList<ClientListData> clientData = new ArrayList<>();
         ListView listView;
 
-        NetworkDiscovery(String localIp, ListView list, ArrayList<ClientListData> clientData) {
+        NetworkDiscovery(String localIp, ListView list) {
             this.localIp = localIp;
             this.listView = list;
-            this.clientData = clientData;
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
-            Log.d(TAG, "Let's sniff the network");
-
             try {
-                Log.d(TAG, "ipString: " + localIp);
-
                 String prefix = localIp.substring(0, localIp.lastIndexOf(".") + 1);
                 Log.d(TAG, "prefix: " + prefix);
 
-                for (int i = 0; i < 255; i++) {
+                for (int i = 2; i < 255; i++) {
                     String testIp = prefix + i;
                     InetAddress address = InetAddress.getByName(testIp);
                     boolean reachable = address.isReachable(256);
                     String hostName = address.getCanonicalHostName();
-
+                    if (testIp.equalsIgnoreCase(localIp)) continue;
+                    publishProgress(null, (int) (i * 100 / 255));
                     if (reachable) {
                         ClientListData clientListData = new ClientListData("Mobile 1", "20%", testIp);
                         Log.i(TAG, "Host: " + hostName + "(" + testIp + ") is reachable!");
-                        publishProgress(clientListData);
-                    } else
-                        Log.e(TAG, "Host: " + hostName + "(" + testIp + ") is not reachable!");
+                        publishProgress(clientListData, (i * 100 / 255));
+                    }
                 }
             } catch (Throwable t) {
                 Log.e(TAG, "Well that's not good.", t);
