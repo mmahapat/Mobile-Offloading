631955aa4eec51bbac9cadde7e8bc38852598ed6
mmahapat <mmahapat@asu.edu>
2020-05-02 06:38:13 -0700

    TaskMonitor Update

--- Master/app/src/main/java/com/mobilespark/master/TaskMonitor.java
+++ Master/app/src/main/java/com/mobilespark/master/TaskMonitor.java
@@ -13,20 +13,36 @@ public class TaskMonitor extends AppCompatActivity {
 
     private ListView activeServerslist;
     private ListView fallbackServerslist;
+    private ArrayList<ClientListData> clientData;
+    private ArrayList<ClientListData> activeClientData;
+    private ArrayList<ClientListData> fallbackClientData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_monitor);
-        ArrayList<ClientListData> clientData = ClientList.clientData;
+        clientData = ClientList.clientData;
+        int Slaves = getIntent().getExtras().getInt("Slaves");
+
         activeServerslist = findViewById(R.id.activeServers);
         fallbackServerslist = findViewById(R.id.fallbackServers);
 
-        ClientListAdapter activeServerAdapter = new ClientListAdapter(this, clientData);
+        int i = 0;
+        activeClientData = new ArrayList<>();
+        while(i < Slaves && i < clientData.size())
+            activeClientData.add(clientData.get(i++));
+
+        fallbackClientData = new ArrayList<>();
+        while(i < clientData.size())
+            fallbackClientData.add(clientData.get(i++));
+
+        ClientListAdapter activeServerAdapter = new ClientListAdapter(this, activeClientData);
         activeServerslist.setAdapter(activeServerAdapter);
 
-        ClientListAdapter fallbackServerAdapter = new ClientListAdapter(this, clientData);
+        ClientListAdapter fallbackServerAdapter = new ClientListAdapter(this, fallbackClientData);
         fallbackServerslist.setAdapter(fallbackServerAdapter);
+
+        //((ClientListAdapter) (activeServerslist.getAdapter())).notifyDataSetChanged();
 
     }
 }
