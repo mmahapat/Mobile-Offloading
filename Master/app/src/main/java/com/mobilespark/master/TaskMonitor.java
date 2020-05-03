dfafbcf3422a3a6d520d897094bb08a879e5e926
mmahapat <mmahapat@asu.edu>
2020-05-03 01:40:40 -0700

    Task Monitor Update

--- Master/app/src/main/java/com/mobilespark/master/TaskMonitor.java
+++ Master/app/src/main/java/com/mobilespark/master/TaskMonitor.java
@@ -5,22 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.android.volley.VolleyError;
 import com.mobilespark.master.Pojos.ClientListData;
+import com.mobilespark.master.WebUtils.VolleyController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class TaskMonitor extends AppCompatActivity {
+public class TaskMonitor extends AppCompatActivity implements ClientResponse {
 
+    private static final String TAG = "TaskMonitor";
     private ListView activeServerslist;
     private ListView fallbackServerslist;
     private ArrayList<ClientListData> clientData;
     private ArrayList<ClientListData> activeClientData;
     private ArrayList<ClientListData> fallbackClientData;
+    private Map<String, Integer> activeClientMap;
+    private Map<String, Integer> fallbackClientMap;
     private Button _assignTaskButton;
 
     @Override
@@ -37,12 +49,18 @@ public class TaskMonitor extends AppCompatActivity {
         int i = 0;
 
         activeClientData = new ArrayList<>();
-        while(i < Slaves && i < clientData.size())
+        fallbackClientData = new ArrayList<>();
+        activeClientMap = new HashMap<>();
+        fallbackClientMap = new HashMap<>();
+
+
+        while (i < Slaves && i < clientData.size())
             activeClientData.add(clientData.get(i++));
 
-        fallbackClientData = new ArrayList<>();
-        while(i < clientData.size())
+
+        while (i < clientData.size())
             fallbackClientData.add(clientData.get(i++));
+
 
         ClientListAdapter activeServerAdapter = new ClientListAdapter(this, activeClientData);
         activeServerslist.setAdapter(activeServerAdapter);
@@ -50,37 +68,76 @@ public class TaskMonitor extends AppCompatActivity {
         ClientListAdapter fallbackServerAdapter = new ClientListAdapter(this, fallbackClientData);
         fallbackServerslist.setAdapter(fallbackServerAdapter);
 
+        Handler handler = new Handler();
+        handler.postDelayed(task, 2000);
 
-
-
-
-        //((ClientListAdapter) (activeServerslist.getAdapter())).notifyDataSetChanged();
         _assignTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                System.out.println("To do /calculate");
-
-                for(int count = 0 ; count < activeClientData.size();count++)
-                    activeServerslist.getChildAt(count).setBackgroundColor(Color.YELLOW);
-                for(int count = 0 ; count < fallbackClientData.size();count++)
-                    fallbackServerslist.getChildAt(count).setBackgroundColor(Color.GRAY);
 
                 ((ClientListAdapter) (activeServerslist.getAdapter())).notifyDataSetChanged();
                 ((ClientListAdapter) (fallbackServerslist.getAdapter())).notifyDataSetChanged();
 
 
                 for(int count = 0 ; count < activeClientData.size();count++) {
-
-                    activeServerslist.getChildAt(count).setBackgroundColor(Color.GREEN);
-
+                    try {
+                        String url = "http://" + activeClientData.get(count).clientIp + ":8080/calculate";
+                        VolleyController volleyController = new VolleyController(getApplicationContext());
+                        JSONObject body = new JSONObject();
+                        body.put("startX", 0);
+                        body.put("endX", 4);
+                        body.put("startY", 5);
+                        body.put("endY", 9);
+                        volleyController.makeRequest(url, body, TaskMonitor.this);
+                    }
+                    catch (Throwable t) {
+                        Log.e(TAG, "Well that's not good.", t);
+                    }
                 }
+
+
 
                 //fallbackServerslist.setBackgroundColor(Color.BLUE);
                 //activeServerslist.setBackgroundColor(Color.parseColor("#9fe7ff"));
                 //((ClientListAdapter) (activeServerslist.getAdapter())).notifyDataSetChanged();
             }
         });
+
+    }
+
+    private Runnable task = new Runnable() {
+        public void run() {
+            for (int count = 0; count < activeClientData.size(); count++) {
+                activeServerslist.getChildAt(count).setBackgroundColor(Color.YELLOW);
+                activeClientMap.put(activeClientData.get(count).clientIp, count);
+            }
+            for (int count = 0; count < fallbackClientData.size(); count++) {
+                fallbackServerslist.getChildAt(count).setBackgroundColor(Color.GRAY);
+                fallbackClientMap.put(fallbackClientData.get(count).clientIp, count);
+            }
+        }
+    };
+
+
+    @Override
+    public void onSuccess(JSONObject jsonObject) {
+        String clientIp = "Empty";
+
+        try {
+            clientIp = (String) jsonObject.get("ip");
+
+        } catch (JSONException e) {
+            Log.e(TAG, "onSuccess: " + "Could not pass JSON");
+        }
+
+        int index = activeClientMap.get(clientIp);
+        activeServerslist.getChildAt(index).setBackgroundColor(Color.GREEN);
+
+    }
+
+    @Override
+    public void onFailure(VolleyError error) {
 
     }
 }
