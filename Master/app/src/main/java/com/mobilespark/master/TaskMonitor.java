08501ab0ba94163ce0a7706504797ed8e6d704a7
mmahapat <mmahapat@asu.edu>
2020-05-02 23:12:28 -0700

    Task Monitor Changes

--- Master/app/src/main/java/com/mobilespark/master/TaskMonitor.java
+++ Master/app/src/main/java/com/mobilespark/master/TaskMonitor.java
@@ -3,6 +3,7 @@ package com.mobilespark.master;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.ListView;
 import com.mobilespark.master.Pojos.ClientListData;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class TaskMonitor extends AppCompatActivity {
 
@@ -33,6 +35,7 @@ public class TaskMonitor extends AppCompatActivity {
         _assignTaskButton = findViewById(R.id.assignbutton);
 
         int i = 0;
+
         activeClientData = new ArrayList<>();
         while(i < Slaves && i < clientData.size())
             activeClientData.add(clientData.get(i++));
@@ -47,12 +50,35 @@ public class TaskMonitor extends AppCompatActivity {
         ClientListAdapter fallbackServerAdapter = new ClientListAdapter(this, fallbackClientData);
         fallbackServerslist.setAdapter(fallbackServerAdapter);
 
+
+
+
+
         //((ClientListAdapter) (activeServerslist.getAdapter())).notifyDataSetChanged();
         _assignTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 System.out.println("To do /calculate");
+
+                for(int count = 0 ; count < activeClientData.size();count++)
+                    activeServerslist.getChildAt(count).setBackgroundColor(Color.YELLOW);
+                for(int count = 0 ; count < fallbackClientData.size();count++)
+                    fallbackServerslist.getChildAt(count).setBackgroundColor(Color.GRAY);
+
+                ((ClientListAdapter) (activeServerslist.getAdapter())).notifyDataSetChanged();
+                ((ClientListAdapter) (fallbackServerslist.getAdapter())).notifyDataSetChanged();
+
+
+                for(int count = 0 ; count < activeClientData.size();count++) {
+
+                    activeServerslist.getChildAt(count).setBackgroundColor(Color.GREEN);
+
+                }
+
+                //fallbackServerslist.setBackgroundColor(Color.BLUE);
+                //activeServerslist.setBackgroundColor(Color.parseColor("#9fe7ff"));
+                //((ClientListAdapter) (activeServerslist.getAdapter())).notifyDataSetChanged();
             }
         });
 
